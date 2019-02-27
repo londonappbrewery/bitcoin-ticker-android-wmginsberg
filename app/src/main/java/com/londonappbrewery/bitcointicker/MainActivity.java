59d62f169b7c5cb2d0d861798ad937f65de7b176
Wendy Ginsberg <wmginsberg@wmginsberg-macbookpro2.roam.corp.google.com>
2019-02-27 16:45:38 +0000

    completing challenge project

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,19 +12,27 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+    private final String LOGCAT_TAG = "Bitcoin";
 
     // Member Variables:
     TextView mPriceTextView;
+    String mNewCurrencyCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,32 +53,56 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(LOGCAT_TAG, "" + adapterView.getItemAtPosition(i));
+                mNewCurrencyCode = adapterView.getItemAtPosition(i).toString();
+                Log.d(LOGCAT_TAG, "> " + mNewCurrencyCode);
+                letsDoSomeNetworking(BASE_URL+mNewCurrencyCode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d(LOGCAT_TAG,"Nothing selected");
+            }
+        });
 
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        //params.put("symbol",mNewCurrencyCode);
+        Log.d(LOGCAT_TAG,url);
+        client.get(url, params, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d(LOGCAT_TAG, "JSON: " + response.toString());
 //                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
 //                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
+                try {
+                    mPriceTextView.setText(response.getString("last"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d(LOGCAT_TAG, "Request fail! Status code: " + statusCode);
+                Log.d(LOGCAT_TAG, "Fail response: " + response);
+                Log.e("ERROR", e.toString());
 //                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            }
+        });
 
 
     }
